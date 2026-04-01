c46bbe74abc7b9bb1b7bcd7110db3a99eeee2e62
jyepesa <juank_713@hotmail.com>
2026-04-01 15:06:07 +0200

    adjust vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+const repoName = "shopping-list"
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
